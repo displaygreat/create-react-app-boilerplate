b7e3b65fb0889b7a88677929a1b47f9611125c8b
displaygreat <displaygreat@gmail.com>
2021-10-13 22:14:23 +0300

    create-react-app-boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My Best App</h1>
+    </div>
+  );
+}
+export default App;
